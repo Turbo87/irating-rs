a827817495ba3bff5e80f228038a517cc257f99a
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-28 21:55:40 +0100

    Simplify example code

--- examples/example.rs
+++ examples/example.rs
@@ -1,35 +1,23 @@
 fn main() {
     let race_results = vec![
-        ("Driver 1", 1, 7526, true).into(),
-        ("Driver 2", 2, 5982, true).into(),
-        ("Driver 3", 3, 5463, true).into(),
-        ("Driver 4", 4, 4279, true).into(),
-        ("Driver 5", 5, 4137, true).into(),
-        ("Driver 6", 6, 4044, true).into(),
-        ("Driver 7", 7, 3891, true).into(),
-        ("Driver 8", 8, 3612, true).into(),
-        ("Driver 9", 9, 3147, true).into(),
-        ("Driver 10", 10, 2823, true).into(),
-        ("Driver 11", 11, 2715, true).into(),
-        ("Driver 12", 12, 2603, true).into(),
-        ("Driver 13", 13, 2512, true).into(),
-        ("Driver 14", 14, 2352, false).into(),
-        ("Driver 15", 15, 2227, true).into(),
-        ("Driver 16", 16, 2195, true).into(),
-        ("Driver 17", 17, 2166, true).into(),
-        ("Driver 18", 18, 2089, true).into(),
-        ("Driver 19", 19, 1773, true).into(),
-        ("Driver 20", 20, 1772, true).into(),
-        ("Driver 21", 21, 1752, true).into(),
-        ("Driver 22", 22, 1748, true).into(),
-        ("Driver 23", 23, 1705, true).into(),
-        ("Driver 24", 24, 1662, true).into(),
-        ("Driver 25", 25, 1622, true).into(),
-        ("Driver 26", 26, 1537, true).into(),
-        ("Driver 27", 27, 1464, true).into(),
-        ("Driver 28", 28, 1203, true).into(),
+        ("Driver 1", 1, 3203, true).into(),
+        ("Driver 2", 2, 3922, true).into(),
+        ("Driver 3", 3, 2974, true).into(),
+        ("Driver 4", 4, 1739, true).into(),
+        ("Driver 5", 5, 1250, true).into(),
+        ("Driver 6", 6, 2588, false).into(),
     ];
 
-    let result = irating::calculate(race_results);
-    dbg!(&result);
+    let results = irating::calculate(race_results);
+    for result in results {
+        println!(
+            "#{}  {}: {} -> {} ({}{})",
+            result.race_result.finish_rank,
+            result.race_result.driver,
+            result.race_result.start_irating,
+            result.new_irating,
+            if result.irating_change > 0. { "+" } else { "" },
+            result.irating_change
+        );
+    }
 }
